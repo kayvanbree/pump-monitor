b390245d8d013f0347966f6fe96f3539d95ed5bb
Kay van Bree <kayvanbree@gmail.com>
2021-01-21 21:14:12 +0100

    Try this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {environment} from '../environments/environment';
 
 @Component({
@@ -6,6 +6,10 @@ import {environment} from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = environment.herokuAppName;
+export class AppComponent implements OnInit {
+  title = '';
+  
+  public ngOnInit(): void {
+    this.title = environment.herokuAppName;
+  }
 }
