518e55723e2a699d4171433201f3e1be7961d9ad
Kay van Bree <kayvanbree@gmail.com>
2021-02-02 18:42:52 +0100

    Back to the old environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,6 +19,6 @@ export const environment = {
   production: true,
   herokuAppName: NG_ENV.HEROKU_APP_NAME || 'Pump Monitor - Localhost',
   oktaBaseUrl: NG_ENV.OKTA_BASE_URL || 'https://dev-1941070.okta.com',
-  oktaClientId: NG_ENV.OKTA_CLIENT_ID || '0oa4xbtiohGG1noeV5d6',
+  oktaClientId: NG_ENV.OKTA_CLIENT_ID || '0oa4x30y96X48XHNU5d6',
   oktaRedirectUri: NG_ENV.OKTA_REDIRECT_URI || 'https://pump-monitor-staging.herokuapps.com/callback',
 };
